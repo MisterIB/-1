ad4c594adaea055e3ff7725f8e075fecf81e6372
MisterIB <163545760+MisterIB@users.noreply.github.com>
2024-10-06 23:34:10 +0700

    Create Queue.h

---
+++ Queue.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+
+template <typename T>
+struct Queue {
+private:
+	template <typename U>
+	struct Node {
+		U value;
+		Node<U>* next;
+
+		Node(U value): value(value), next(nullptr) {}
+	};
+
+	Node<T>* head;
+	Node<T>* tail;
+
+public:
+	Queue(): head(nullptr), tail(nullptr) {}
+
+	bool is_empty() {
+		return head == nullptr;
+	}
+
+	void push(T value) {
+		Node<T>* newNode = new Node<T>(value);
+		if (is_empty()) {
+			head = newNode;
+			tail = newNode;
+			return;
+		}
+		tail->next = newNode;
+		tail = newNode;
+	}
+
+	T pop() {
+		if (is_empty()) return NULL;
+		Node<T>* temp = head;
+		head = head->next;
+		T value = temp->value;
+		delete temp;
+		return value;
+	}
+
+	int32_t size() {
+		int32_t size = 0;
+		Node<T>* tempNode = head;
+		while (tempNode) {
+			tempNode = tempNode->next;
+			size++;
+		}
+		return size;
+	}
+
+	void print() {
+		Node<T>* tempNode = head;
+		while (tempNode) {
+			std::cout << tempNode->value << " ";
+			tempNode = tempNode->next;
+		}
+		std::cout << std::endl;
+	}
+
+	void printInFile(std::ofstream& file) {
+		Node<T>* tempNode = head;
+		while (tempNode) {
+			file << tempNode->value << " ";
+			tempNode = tempNode->next;
+		}
+		file << std::endl;
+	}
+};
